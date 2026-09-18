00d22182ba8cf805d65489b29e4d6ea88c013292
Mariam-Ghoneem <mariam.ali@halan.com>
2026-09-18 17:44:31 +0300

    create contact test

---
+++ src/test/java/tests/CreateContactTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import base.TestBase;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import models.Contact;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.ContactDetailsPage;
+import pages.ContactsListPage;
+import utils.ContactDataGenerator;
+
+@Epic("Contacts")
+@Feature("Create Contact")
+public class CreateContactTest extends TestBase {
+
+    @Test
+    @Story("Create contact with valid data")
+    @Description("Validate that a user can successfully create a contact by entering valid data ")
+    public void createContact_withValidData_savesSuccessfully() {
+        Contact contact = ContactDataGenerator.generateValidContact();
+
+        ContactDetailsPage detailsPage = new ContactsListPage(driver)
+                .tapAddContact()
+                .fillAndSave(contact);
+
+        Assert.assertTrue(detailsPage.isContactDisplayed(contact),
+                "Saved contact name was not displayed on the contact details screen");
+    }
+}
